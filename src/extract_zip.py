37ac990f223da24ff250e5508792532b553d655b
Pocket List <deepumo3832@gmail.com>
2026-09-03 17:20:39 +0530

    Update extract_zip.py

--- src/extract_zip.py
+++ src/extract_zip.py
@@ -1,11 +1,19 @@
-import os, zipfile
+import os
+import zipfile
+import glob
+
+
+def find_zip_file(directory="assets"):
+    files = glob.glob(os.path.join(directory, "*.zip"))
+    if not files:
+        raise FileNotFoundError("No .zip file found in 'assets' folder. Please add your images zip file.")
+    return files[0]
+
 
 def extract_input_zip(zip_path, output_dir="assets/images"):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    if not os.path.exists(zip_path):
-        raise FileNotFoundError(f"Zip file not found: {zip_path}")
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(output_dir)
-    print(f"✅ Images extracted to {output_dir}")
+    print(f"Images extracted from {zip_path} to {output_dir}")
     return output_dir
